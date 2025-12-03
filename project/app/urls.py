ea62feed69be8afcd4582cede4a2d6f5b8073c6d
LCL <1921699889@qq.com>
2025-12-03 11:17:50 +0800

    1

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('get_data', get_data, name='get_data'),
     path('menu_data', menu_data, name='menu_data'),
     path('rack_power', rack_power, name='rack_power'),
     path('rack_power_excel', rack_power_excel, name='rack_power_excel'),
--- project/app/views.py
+++ project/app/views.py
@@ -19,14 +19,38 @@ logging.basicConfig(
 )
 
 import atexit
-from clickhouse_driver import Client
 import time
 import pandas as pd
 
+from clickhouse_driver import Client
+
 class Connect_Clickhouse:
 
-    def __init__(self,config):
-        self.config=config
+    def __init__(self):
+        self.config={
+            "connection":{
+                "TIMES":3,
+                "TIME":1
+            },
+            "clickhouse":{
+                "HOST":"10.216.140.107",
+                "PORT":9000,
+                "USERNAME":"default",
+                "PASSWORD":""
+            }
+        }
+        self.config={
+            "connection":{
+                "TIMES":3,
+                "TIME":0.1
+            },
+            "clickhouse":{
+                "HOST":"localhost",
+                "PORT":5001,
+                "USERNAME":"default",
+                "PASSWORD":""
+            }
+        }
         self.client=self.login()
         atexit.register(self.close)
 
@@ -79,6 +103,18 @@ class Connect_Mongodb:
                 "PASSWORD":"cds-cloud@2017"
             }
         }
+        self.config={
+            "connection":{
+                "TIMES":1000,
+                "TIME":0.1
+            },
+            "mongodb":{
+                "HOST":"localhost",
+                "PORT":4000,
+                "USERNAME":"manager",
+                "PASSWORD":"cds-cloud@2017"
+            }
+        }
         self.client=self.login()
         self.db=self.get_database()
         atexit.register(self.close)
@@ -123,54 +159,106 @@ class Connect_Mongodb:
         logging.error(f"{name}数据获取失败。")
         raise Exception(f"{name}数据获取失败。")
 
-config={
-    "connection":{
-        "TIMES":1000,
-        "TIME":0.1
-    },
-    "clickhouse":{
-        "HOST":"10.216.140.107",
-        "PORT":9000,
-        "USERNAME":"default",
-        "PASSWORD":""
-    }
-}
-# config={
-#     "connection":{
-#         "TIMES":3,
-#         "TIME":0.1
-#     },
-#     "clickhouse":{
-#         "HOST":"localhost",
-#         "PORT":5001,
-#         "USERNAME":"default",
-#         "PASSWORD":""
-#     }
-# }
+from bson import ObjectId
+
+def get_relationship(city_ObjectId):
+    conn=Connect_Mongodb()
+    pipeline=[
+        {
+            '$match':{
+                'status':1,
+                'asset_status':{
+                    '$in':[
+                        ObjectId("5f964e31df0dfd65aaa716ec"),
+                        ObjectId("5fcef6de94103c791bc2a471")
+                    ]
+                }
+            }
+        },
+        {
+            '$lookup':{
+                'from':'cds_ci_location_detail',
+                'localField':'_id',
+                'foreignField':'device_id',
+                'as':'location'
+            }
+        },
+        {
+            '$match':{
+                'location.status':1
+            }
+        },
+        {
+            '$project':{
+                "hostname":1
+            }
+        }
+    ]
+    jh1=set(pd.DataFrame(list(conn.db.cds_ci_att_value_network.aggregate(pipeline))).astype(str)["hostname"].values.tolist())
+    pipeline[2]["$match"]['device_server_group']={'$ne':ObjectId("5ec8c70a94285cfd9cacee9b")}
+    pipeline[2]["$match"]['device_server_type']={'$ne':ObjectId("5ec8c70a94285cfd9caceebf")}
+    jh2=set(pd.DataFrame(list(conn.db.cds_ci_att_value_server.aggregate(pipeline))).astype(str)["hostname"].values.tolist())
+    data1=conn.get_collection("cds_ci_location_detail",{"status":1,"ci_name":"network","position_id":ObjectId(city_ObjectId)},{"position_id":1,"data_center_id":1,"room_id":1,"rack_id":1,"device_id":1})[["position_id","data_center_id","room_id","rack_id","device_id"]].values.tolist()
+    data2=conn.get_collection("cds_ci_location_detail",{"status":1,"ci_name":"server","position_id":ObjectId(city_ObjectId)},{"position_id":1,"data_center_id":1,"room_id":1,"rack_id":1,"device_id":1})[["position_id","data_center_id","room_id","rack_id","device_id"]].values.tolist()
+    city=conn.get_collection("cds_ci_att_value_position",{"status":1},{"_id":1,"city":1})[["_id","city"]].values.tolist()
+    city_dict=dict(zip([i[0] for i in city],[i[1] for i in city]))
+    data_center=conn.get_collection("cds_ci_att_value_data_center",{"status":1},{"_id":1,"data_center_name":1})[["_id","data_center_name"]].values.tolist()
+    data_center_dict=dict(zip([i[0] for i in data_center],[i[1] for i in data_center]))
+    room=conn.get_collection("cds_ci_att_value_room",{"status":1},{"_id":1,"room_name":1})[["_id","room_name"]].values.tolist()
+    room_dict=dict(zip([i[0] for i in room],[i[1] for i in room]))
+    rack=conn.get_collection("cds_ci_att_value_rack",{"status":1},{"_id":1,"rack_name":1})[["_id","rack_name"]].values.tolist()
+    rack_dict=dict(zip([i[0] for i in rack],[i[1] for i in rack]))
+    network=conn.get_collection("cds_ci_att_value_network",{"status":1},{"_id":1,"hostname":1,"device_ip":1,"brand":1})[["_id","hostname","device_ip","brand"]].values.tolist()
+    network_dict=dict(zip([i[0] for i in network],[(i[1],i[2],i[3],"network") for i in network]))
+    server=conn.get_collection("cds_ci_att_value_server",{"status":1},{"_id":1,"hostname":1,"out_band_ip":1,"web_brand":1})[["_id","hostname","out_band_ip","web_brand"]].values.tolist()
+    server_dict=dict(zip([i[0] for i in server],[(i[1],i[2],i[3],"server") for i in server]))
+    data=[]
+    for i in data1:
+        temp=[city_dict.get(i[0],None),data_center_dict.get(i[1],None),room_dict.get(i[2],None),rack_dict.get(i[3],None),network_dict.get(i[4],None)]
+        if None in temp:
+            continue
+        if temp[-1][0] not in jh1:
+            continue
+        data.append(temp)
+    for i in data2:
+        temp=[city_dict.get(i[0],None),data_center_dict.get(i[1],None),room_dict.get(i[2],None),rack_dict.get(i[3],None),server_dict.get(i[4],None)]
+        if None in temp:
+            continue
+        if temp[-1][0] not in jh2:
+            continue
+        data.append(temp)
+    zd={}
+    for i in data:
+        s="|".join(i[:-1])
+        if s not in zd:
+            zd[s]=[]
+        zd[s].append(i[-1])
+    temp=[]
+    for i in zd:
+        if not zd[i]:
+            temp.append(i)
+    for i in temp:
+        del zd[i]
+    return zd
+
+def get_ObjectId(city_name):
+    conn=Connect_Mongodb()
+    city_ObjectId=conn.get_collection("cds_ci_att_value_position",{"status":1,"city":city_name},{"_id":1})["_id"].values.tolist()[0]
+    return city_ObjectId
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-@api_view(['POST'])
-def get_data(request):
-    return Response(request.data)
-
-from datetime import datetime,timedelta
 
 @api_view(['GET'])
 def menu_data(request):
     zd={}
     zd["code"]=200;zd["msg"]=""
-    conn=Connect_Clickhouse(config)
-    client=conn.client
-    end_time=datetime.now()+timedelta(days=1)
-    start_time=end_time-timedelta(days=2)
-    start_str=start_time.strftime('%Y-%m-%d %H:%M:%S')
-    end_str=end_time.strftime('%Y-%m-%d %H:%M:%S')
-    query=f'''
-    SELECT city,data_center,room,rack FROM power.power_data WHERE ts >='{start_str}' AND ts<='{end_str}'
-    '''
-    data=conn.query(query).values.tolist()
+    data=[]
+    for city in ["庆阳","达拉斯","台北"]:
+        temp=get_relationship(get_ObjectId(city))
+        for _ in list(temp.keys()):
+            _=_.split("|")
+            data.append(_)
     temp={}
     for i in data:
         a,b,c,d=i[0],i[1],i[2],i[3]
@@ -212,7 +300,7 @@ def rack_power(request):
     query=f'''
     SELECT voltage,current,power,ts FROM power.power_data WHERE ts >='{begin_time}' AND ts<='{end_time}' AND city='{city}' AND data_center='{data_center}' AND room='{room}' AND rack='{rack}' ORDER BY ts ASC
     '''
-    conn=Connect_Clickhouse(config)
+    conn=Connect_Clickhouse()
     client=conn.client
     data=conn.query(query)[["voltage","current","power","ts"]].values.tolist()
     temp={}
@@ -265,7 +353,7 @@ def rack_power_excel(request):
     query=f'''
     SELECT voltage,current,power,ts FROM power.power_data WHERE ts >='{begin_time}' AND ts<='{end_time}' AND city='{city}' AND data_center='{data_center}' AND room='{room}' AND rack='{rack}' ORDER BY ts ASC
     '''
-    conn=Connect_Clickhouse(config)
+    conn=Connect_Clickhouse()
     client=conn.client
     data=conn.query(query)[["voltage","current","power","ts"]].values.tolist()
     temp={}
@@ -305,7 +393,7 @@ def power_csv_all(request):
     query=f'''
     SELECT * FROM power.power_data WHERE ts >='{begin_time}' AND ts<='{end_time}' ORDER BY ts ASC LIMIT {str(batch_size)} OFFSET 
     '''
-    conn=Connect_Clickhouse(config)
+    conn=Connect_Clickhouse()
     client=conn.client
     temp_dir=os.path.join(os.getcwd(),"temp_files")
     os.makedirs(temp_dir,exist_ok=True)
@@ -353,7 +441,7 @@ def rack_power_list(request):
     query=f'''
     SELECT voltage,current,power,ts,hostname,type,ip FROM power.power_data WHERE ts >='{begin_time}' AND ts<='{end_time}' AND city='{city}' AND data_center='{data_center}' AND room='{room}' AND rack='{rack}' ORDER BY ts ASC
     '''
-    conn=Connect_Clickhouse(config)
+    conn=Connect_Clickhouse()
     client=conn.client
     data=conn.query(query)[["voltage","current","power","ts","ip","hostname","type"]].values.tolist()
     zd_temp={}
@@ -395,7 +483,7 @@ def rack_power_list_excel(request):
     query=f'''
     SELECT voltage,current,power,ts,hostname FROM power.power_data WHERE ts >='{begin_time}' AND ts<='{end_time}' AND city='{city}' AND data_center='{data_center}' AND room='{room}' AND rack='{rack}' ORDER BY ts ASC
     '''
-    conn=Connect_Clickhouse(config)
+    conn=Connect_Clickhouse()
     client=conn.client
     data=conn.query(query)[["voltage","current","power","ts","hostname"]].values.tolist()
     zd_temp={}
@@ -458,7 +546,7 @@ def power_csv_all_more(request):
     ORDER BY period_start, hostname
     LIMIT {batch_size} OFFSET 
     '''
-    conn=Connect_Clickhouse(config)
+    conn=Connect_Clickhouse()
     client=conn.client
     temp_dir=os.path.join(os.getcwd(),"temp_files")
     os.makedirs(temp_dir,exist_ok=True)
